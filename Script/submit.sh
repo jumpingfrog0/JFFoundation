1dcfb541505f8721c0d0af8583997d4b70826131
jumpingfrog0 <jumpingfrog0@gmail.com>
2018-09-10 19:25:56 +0800

    fix push tag

--- Script/submit.sh
+++ Script/submit.sh
@@ -35,6 +35,6 @@ git push origin master
 echo "==> 提交 tag $VERSION"
 git tag -d $VERSION
 git tag $VERSION -m "tag: $VERSION"
-git push --tags
+git push --force origin refs/tags/$VERSION:refs/tags/$VERSION
 
 pod repo push $POD_REPO $POD_SPEC --allow-warnings --verbose --sources=$POD_REPO
